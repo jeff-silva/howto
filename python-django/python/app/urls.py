afbe8da0f4d7d9d1bbb6bf1c762a6e8f0e87598a
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-10-03 12:09:25 -0300

    feat: python-django

--- python-django/python/app/urls.py
+++ python-django/python/app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for src project.
+URL configuration for app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
-
 urlpatterns = [
+    path('', lambda request: HttpResponse('Home')),
     path('admin/', admin.site.urls),
-    path('aaa/', lambda request: HttpResponse('{"hello": "World"}')),
+    path('todo/', include('todo.urls')),
 ]
