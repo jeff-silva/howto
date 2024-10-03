9dc9cf39568811909f9b7f87c55d82d637f433a4
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-10-03 19:47:43 -0300

    feat: python-django

--- python-django/python/todo/admin.py
+++ python-django/python/todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+
+@admin.register(Todo)
+
+class TodoAdmin(admin.ModelAdmin):
+  list_display=('name', 'finalized_at', 'created_at', 'updated_at')
--- python-django/python/todo/models.py
+++ python-django/python/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+  name = models.CharField(max_length=255)
+  slug = models.CharField(max_length=255, unique=True)
+  finalized_at = models.DateTimeField(blank=True, null=True, default=None)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  deleted_at = models.DateTimeField(blank=True, null=True, default=None)
+
+  def __str__(self):
+    return self.name
