05ff2d1f341b465d25e66afbb6d91313a9056e3d
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-10-03 01:54:42 -0300

    feat: python-django

--- python-django/python/src/urls.py
+++ python-django/python/src/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('aaa/', lambda request: HttpResponse('{"hello": "World"}')),
 ]
