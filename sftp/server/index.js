ea98a084e0c9cf60079a14d8bff0344bcaf6ea1d
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-10-22 02:11:17 -0300

    feat: sftp: configuração

--- sftp/server/index.js
+++ sftp/server/index.js
@@ -1,10 +1,33 @@
 "use strict";
 
+// TODO: Verificar Senha da conexão
+// TODO: Verificar IP da conexão
+// TODO: Bloquear conexão de acordo com users.json
+
 const fs = require("fs");
 const ssh2 = require("ssh2");
 const SftpServer = require("./lib/ssh2-sftp-server/index.js");
-// const SftpServer = require("ssh2-sftp-server");
-const _log = console.log;
+
+if (!fs.existsSync("./uploads/config.json")) {
+  fs.writeFileSync(
+    "./uploads/config.json",
+    JSON.stringify(
+      {
+        users: [
+          {
+            user: "main",
+            pass: "main",
+            ips: [],
+          },
+        ],
+      },
+      null,
+      2
+    )
+  );
+}
+
+const config = require("./uploads/config.json");
 
 const options = {
   hostKeys: [fs.readFileSync(`${__dirname}/ssh/id_rsa`)],
@@ -13,8 +36,15 @@ const options = {
 
 let srv = new ssh2.Server(options, (client) => {
   client.on("authentication", (ctx) => {
-    console.log(ctx.username);
-    ctx.accept();
+    if (ctx.method == "password") {
+      config.users.map((user) => {
+        if (ctx.username == user.user && ctx.password == user.pass) {
+          return ctx.accept();
+        }
+      });
+    }
+
+    return ctx.reject();
   });
 
   client.on("ready", () => {
@@ -31,5 +61,5 @@ let srv = new ssh2.Server(options, (client) => {
 srv.on("close", () => null);
 
 srv.listen(22, () => {
-  _log("Listening on port " + srv.address().port);
+  console.log("Listening on port " + srv.address().port);
 });
