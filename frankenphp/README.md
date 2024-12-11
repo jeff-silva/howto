3fc8d2a2d442993377b15ea9dd02f8f94f7aac7b
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-12-11 20:30:59 -0300

    feat: frankenphp

---
+++ frankenphp/README.md
@@ -0,0 +1,8 @@
+# frankenphp
+
+How to run:
+
+```bash
+cd ./frankenphp && yarn dev
+```
+
---
+++ frankenphp/app/nav.php
@@ -0,0 +1,5 @@
+<ul style="display:flex; gap:5px; justify-content:center; list-style-type:none; margin:0 auto;">
+  <li><a href="https://localhost:80">Site80</a></li>
+  <li><a href="https://localhost:81">Site81</a></li>
+  <li><a href="https://localhost:82">Site82</a></li>
+</ul>
