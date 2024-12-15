c11d18d354e92331712e278dbc5d8e8eedaa2a8f
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-12-14 22:18:50 -0300

    feat: Melhorias SFTPGo

--- frankenphp/app/public/index.php
+++ frankenphp/app/public/index.php
@@ -1,1 +1,13 @@
-<?php phpinfo();
+<?php ob_start(); ?>
+<?php include __DIR__ . '/../nav.php'; ?>
+<h1 style="text-align:center;">Site80</h1>
+
+
+<?php
+$content = ob_get_clean();
+
+ob_start();
+phpinfo();
+$phpinfo = ob_get_clean();
+
+echo str_replace('<body>', '<body>' . $content, $phpinfo);
