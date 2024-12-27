a57cbd4f086644b588fc6741599b7db59693a670
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-12-27 17:32:16 -0300

    feat: nuxt-capacitor android

--- nuxt-capacitor/nuxt3/capacitor.config.ts
+++ nuxt-capacitor/nuxt3/capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.labscript.test',
-  appName: 'Test App',
-  webDir: 'www'
+  appId: "com.labscript.test",
+  appName: "Test App",
+  webDir: "www",
 };
 
 export default config;
