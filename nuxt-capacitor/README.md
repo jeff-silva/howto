7b3c96969fd43bece177835a58bec5c9cc37f978
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-12-28 01:06:51 -0300

    fix: nuxt-capacitor: remove

--- nuxt-capacitor/README.md
+++
@@ -1,8 +0,0 @@
-# nuxt-capacitor
-
-How to run:
-
-```bash
-cd ./nuxt-capacitor && yarn dev
-```
-
--- nuxt-capacitor/nuxt3/android/app/src/main/java/com/labscript/test/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.labscript.test;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
