01938a5dbb9819b468cd1f8afe0441b2f282b4e4
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-04-03 19:50:25 -0300

    feat: nestjs-mysql: app_file crud

--- nestjs-mysql/nestjs/app/src/app_file/app_file.entity.ts
+++ nestjs-mysql/nestjs/app/src/app_file/app_file.entity.ts
@@ -1,5 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { IsNumber, IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('app_file')
 export class AppFile {
@@ -8,6 +15,24 @@ export class AppFile {
 
   @Column({ default: null })
   name: string;
+
+  @Column()
+  path: string;
+
+  @Column()
+  size: number;
+
+  @Column()
+  mime: string;
+
+  @Column('longtext')
+  content: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export class AppFileDto {
@@ -16,5 +41,25 @@ export class AppFileDto {
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ type: String, example: 'filename.txt' })
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, example: 'algo.txt' })
+  path: string;
+
+  @IsNumber()
+  @ApiProperty({ type: Number, example: 1024 })
+  size: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, example: 'text/plain' })
+  mime: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ type: String, example: 'base64encodedstring...' })
+  content: string;
 }
