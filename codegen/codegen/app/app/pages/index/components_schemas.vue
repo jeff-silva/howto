f85a3fe02dcb7c3ea9bf1244b78107ae67e049ab
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-08-29 15:13:00 -0300

    feat: codegen

--- codegen/codegen/app/app/pages/index/components_schemas.vue
+++ codegen/codegen/app/app/pages/index/components_schemas.vue
@@ -9,7 +9,7 @@
               <template #append>
                 <v-btn
                   text="Edit"
-                  @click="edit.set(o)"
+                  @click="schema.edit(o)"
                 />
               </template>
             </v-list-item>
@@ -18,15 +18,30 @@
       </v-col>
 
       <v-col cols="9">
-        <v-text-field v-model="edit.value.name" />
+        <v-text-field v-model="schema.value.name" />
+        <div>Properties</div>
+
+        <template v-for="o in schemaProp.items">
+          <v-text-field v-model="o.name" />
+          <pre>{{ o }}</pre>
+        </template>
+
+        <!-- <pre>{{ schema }}</pre> -->
+        <pre>{{ schemaProp }}</pre>
+
+        <!-- <template v-for="o in schema.value.data.properties">
+          <pre>{{ o }}</pre>
+        </template>
         <pre>{{ edit.value }}</pre>
         <v-btn
           text="save"
           @click="schema.save()"
-        />
+        /> -->
       </v-col>
     </v-row>
-    <pre>{{ schema }}</pre>
+
+    <!-- <pre>{{ schemaProp }}</pre> -->
+    <!-- <pre>{{ schema }}</pre> -->
   </div>
 </template>
 
@@ -44,31 +59,54 @@ const model = reactive({
   },
 });
 
-const schema = reactive({
-  items: Object.entries(model.value.components.schemas).map(([name, data]) => ({
-    name,
-    data,
-  })),
-  save() {
-    const data = Object.fromEntries(schema.items.map((o) => [o.name, o.data]));
-    model.value.components.schemas = data;
-    model.emit();
-    console.log("schema.save");
+const useObjectEdit = (opts = {}) => {
+  opts = {
+    items: [],
+    onEdit: (value) => value,
+    onSave: (value) => value,
+    ...opts,
+  };
+
+  const r = reactive({
+    value: null,
+    items: [],
+
+    setItems(items) {
+      if (items !== null && typeof items == "object" && !Array.isArray(items)) {
+        r.items = Object.entries(items || {}).map(([name, data]) => ({
+          name,
+          data,
+        }));
+      }
+      return r.items;
+    },
+
+    edit(value) {
+      r.value = opts.onEdit(value);
+    },
+
+    save() {
+      const data = Object.fromEntries(r.items.map((o) => [o.name, o.data]));
+      return opts.onSave(data);
+    },
+  });
+
+  r.setItems(opts.items);
+  r.edit(r.items.at(0) || null);
+  return r;
+};
+
+const schemaProp = useObjectEdit();
+
+const schema = useObjectEdit({
+  items: model.value.components.schemas,
+  // onSave(value) {
+  //   console.log(value);
+  //   return value;
+  // },
+  onEdit(value) {
+    schemaProp.setItems(value.data.properties);
+    return value;
   },
 });
-
-const edit = reactive({
-  value: schema.items.at(0),
-  set(value) {
-    edit.value = value;
-  },
-});
-
-watch(
-  () => edit.value,
-  () => {
-    schema.save();
-  },
-  { deep: true }
-);
 </script>
