e10fc96b254933d5d9ddb4a042b82ddc0552c439
Jeferson Silva <jeferson.i.silva@gmail.com>
2025-09-02 20:05:28 -0300

    feat: playback rec

--- docs-src/app/app/pages/tool/index/playback-rec.vue
+++ docs-src/app/app/pages/tool/index/playback-rec.vue
@@ -2,7 +2,7 @@
   <div>
     <h2>Playback Rec</h2>
     <br />
-    <v-stepper
+    <!-- <v-stepper
       v-model="steps.value"
       :items="steps.items"
     >
@@ -30,14 +30,63 @@
         <br />
         <v-btn
           text="Backtrack"
-          @click="recorder.backtrackInit()"
+          @click="recorder.backtrackSelect()"
         />
       </template>
 
       <template v-slot:item.record>
+        <audio
+          ref="audioRef"
+          controls
+          :src="recorder.meta.backtrackUrl"
+        ></audio>
+
+        <video
+          v-if="recorder.meta.recordedVideoUrl"
+          :src="recorder.meta.recordedVideoUrl"
+          controls
+        ></video>
+
         <div>Gravar</div>
       </template>
-    </v-stepper>
+    </v-stepper> -->
+
+    <v-btn
+      v-if="!recorder.meta.backtrackUrl"
+      text="Selecionar Backtrack"
+      @click="recorder.backtrackSelect()"
+    />
+
+    <video
+      ref="videoRef"
+      autoplay
+    ></video>
+
+    <audio
+      v-if="recorder.meta.backtrackUrl"
+      ref="audioRef"
+      controls
+      :src="recorder.meta.backtrackUrl"
+    ></audio>
+
+    <video
+      v-if="recorder.meta.recordedVideoUrl"
+      ref="recordedRef"
+      :src="recorder.meta.recordedVideoUrl"
+      controls
+    ></video>
+
+    <v-btn
+      v-if="!recorder.meta.multiStreamRecorder"
+      text="Iniciar Gravação"
+      @click="recorder.recordStart()"
+    />
+
+    <v-btn
+      v-if="recorder.meta.multiStreamRecorder"
+      text="Parar Gravação"
+      @click="recorder.recordStop()"
+    />
 
     <pre>recorder: {{ recorder }}</pre>
     <pre>steps: {{ steps }}</pre>
@@ -45,6 +94,12 @@
 </template>
 
 <script setup>
+import { MultiStreamRecorder } from "recordrtc";
+
+const audioRef = ref(null);
+const videoRef = ref(null);
+const recordedRef = ref(null);
+
 const steps = reactive({
   value: "permission",
   items: [
@@ -68,27 +123,70 @@ const steps = reactive({
 const recorder = reactive({
   meta: {
     mediaStream: null,
-    backtrackFile: null,
+    backtrackUrl: null,
+    recordedVideoUrl: null,
+    mediaRecorder: null,
+    multiStreamRecorder: null,
   },
   async permissionInit() {
-    recorder.meta.mediaStream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-    if (recorder.meta.mediaStream) {
-      steps.value = "backtrack";
-    }
+    videoRef.value.srcObject = recorder.meta.mediaStream =
+      await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+    return recorder.meta.mediaStream;
   },
-  async backtrackInit() {
+  async backtrackSelect() {
+    await recorder.permissionInit();
     Object.assign(document.createElement("input"), {
       type: "file",
       onchange: (ev) => {
-        recorder.meta.backtrackFile = ev.target.files[0] || null;
-        if (recorder.meta.backtrackFile) {
-          steps.value = "record";
+        if (ev.target.files[0]) {
+          recorder.meta.backtrackUrl = URL.createObjectURL(ev.target.files[0]);
         }
       },
     }).click();
+  },
+  async recordStart() {
+    audioRef.value.play();
+
+    const videoStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    const combined = new MediaStream();
+    recorder.meta.mediaStream
+      .getVideoTracks()
+      .forEach((t) => combined.addTrack(t));
+
+    const audioCtx = new AudioContext();
+    const destination = audioCtx.createMediaStreamDestination();
+    const mixedAudioStream = destination.stream;
+    mixedAudioStream.getAudioTracks().forEach((t) => combined.addTrack(t));
+
+    const aaa = new MediaStreamRecorder(combined, {
+      mimeType: "video/webm;codecs=vp8,opus",
+    });
+
+    // const chunks = [];
+    // mediaRecorder.ondataavailable = (ev) => {
+    //   if (ev.data && ev.data.size) chunks.push(ev.data);
+    // };
+
+    // mediaRecorder.onstop = () => {
+    //   recorder.meta.recordedVideoUrl = URL.createObjectURL(
+    //     new Blob(chunks, {
+    //       type: "video/webm",
+    //     })
+    //   );
+    // };
+  },
+
+  recordStop() {
+    recorder.meta.multiStreamRecorder.stop((blob) => {
+      console.log(blob);
+    });
   },
 });
 </script>
