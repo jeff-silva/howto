4847fb42010180b5cd451d598a1cae166f3bf535
Jeff Silva <jeferson.i.silva@gmail.com>
2026-05-22 19:32:04 -0300

    feat: integrate Laravel Sanctum for API authentication and initialize movie search controller

---
+++ moviedb/service-app/src/app/Http/Controllers/Mdb/MdbMovieSearchController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Mdb;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MdbMovieSearchController extends Controller
+{
+    //
+}
