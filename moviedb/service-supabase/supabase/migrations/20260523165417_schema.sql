80aaf3450dd377dd9ebbb770d08fbdbf1071b379
Jeff Silva <jeferson.i.silva@gmail.com>
2026-05-23 13:54:37 -0300

    feat: add pgvector extension and embedding column to mdb_movie table

---
+++ moviedb/service-supabase/supabase/migrations/20260523165417_schema.sql
@@ -0,0 +1,33 @@
+create extension if not exists "vector" with schema "extensions";
+
+alter table "public"."mdb_movie" add column "embedding" extensions.vector(384);
+
+grant delete on table "public"."mdb_movie" to "postgres";
+
+grant insert on table "public"."mdb_movie" to "postgres";
+
+grant references on table "public"."mdb_movie" to "postgres";
+
+grant select on table "public"."mdb_movie" to "postgres";
+
+grant trigger on table "public"."mdb_movie" to "postgres";
+
+grant truncate on table "public"."mdb_movie" to "postgres";
+
+grant update on table "public"."mdb_movie" to "postgres";
+
+grant delete on table "public"."mdb_movie_credit" to "postgres";
+
+grant insert on table "public"."mdb_movie_credit" to "postgres";
+
+grant references on table "public"."mdb_movie_credit" to "postgres";
+
+grant select on table "public"."mdb_movie_credit" to "postgres";
+
+grant trigger on table "public"."mdb_movie_credit" to "postgres";
+
+grant truncate on table "public"."mdb_movie_credit" to "postgres";
+
+grant update on table "public"."mdb_movie_credit" to "postgres";
+
+
