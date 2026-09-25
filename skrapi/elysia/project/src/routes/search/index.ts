600267f6b5d5b72378b33eb881ed4826a6fb8c71
Jeff Silva <jeferson.i.silva@gmail.com>
2026-09-25 00:51:00 -0300

    refactor: update search query parameters and response data structure

--- skrapi/elysia/project/src/routes/search/index.ts
+++ skrapi/elysia/project/src/routes/search/index.ts
@@ -4,11 +4,17 @@ export default new Elysia()
   .get("/", async ({ query }) => {
     // Transformamos o objeto (que tem o 'q' e o que mais vier) em formato de URL (?q=xxx&format=json)
     // O 'as Record<string, string>' acalma o TypeScript dizendo que todos os valores serão strings
+    // const params = new URLSearchParams({
+    //   ...query,
+    //   format: 'json',
+    // } as Record<string, string>);
+  
     const params = new URLSearchParams({
-      ...query,
+      q: query.q,
+      pageno: query.page ?? "1",
+      safesearch: "0",
       format: 'json',
     } as Record<string, string>);
-
 
     try {
       const response = await fetch(`http://searxng:8080/search?${params.toString()}`);
@@ -16,9 +22,8 @@ export default new Elysia()
         throw new Error(`Erro HTTP: ${response.status}`);
       }
       
-      // Retorna o JSON direto do SearXNG
-      const data = (await response.json()) as Record<string, any>;
-      const results = (data.results ?? []).map((item: Record<string, any>) => {
+      const resp = (await response.json()) as Record<string, any>;
+      const data = (resp.results ?? []).map((item: Record<string, any>) => {
         return {
           url: item.url,
           title: item.title,
@@ -26,7 +31,8 @@ export default new Elysia()
           thumbnail: item.thumbnail,
         };
       });
-      return results;
+
+      return { data };
       
     } catch (error: any) {
       return {
@@ -38,5 +44,7 @@ export default new Elysia()
   }, {
     query: t.Object({
       q: t.String({ description: "O termo a ser pesquisado" }),
-    }, { additionalProperties: true }) // Isso diz pro TypeBox: "Aceite o 'q' mas não jogue fora os outros parâmetros!"
+      // O t.Optional diz que não é obrigatório. O default preenche sozinho caso o usuário não envie!
+      page: t.Optional(t.String({ description: "Página dos resultados", default: "1" })),
+    }, { additionalProperties: true })
   });
